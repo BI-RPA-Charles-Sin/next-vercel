ce50e47c55d596b7727b2bb6beac79a9ee5aaac3
Charles_Sin <charles_sin@mail.chungyo.net>
2022-12-07 12:58:57 +0800

    add post method

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import NextCors from "nextjs-cors";
+
+export default async function handler(req, res) {
+  const { method } = req;
+
+  if (method == "POST") {
+    res.status(200).json({ name: "POST John Doe" });
+  }
+
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+
+  res.status(200).json({ name: "John Doe" });
 }
